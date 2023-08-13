64c1c017c9fcff743aa00e57fa4609e4455461fd
CoderLLImoder <kaperskiyi@gmail.com>
2023-08-13 09:51:48 +0300

    Init new project
    Setup personal settings

---
+++ HW12/HW12/Sources/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HW12
+//
+//  Created by Илья Капёрский on 11.08.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = UIColor.green
+        // Do any additional setup after loading the view.
+    }
+
+}
+
